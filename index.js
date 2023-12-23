efc926493d2b99ed0b65eb2979791212b0d84ab9
Luiz <seu-email@example.com>
2023-12-23 20:49:43 -0300

    final

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const AuthController = require('./src/controllers/AuthController.js');
 const AdminController = require('./src/controllers/AdminController')
 
 const authenticateMiddleware = require('./src/middlewares/authenticate')
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/auth', AuthController)
