e1a22d68e6636012a6ea3f2824210a7793823a85
Luiz <seu-email@example.com>
2023-12-24 09:19:03 -0300

    final

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const AdminController = require('./src/controllers/AdminController')
 
 const authenticateMiddleware = require('./src/middlewares/authenticate')
 const cors = require('cors');
-const router = require('express').Router();
+const swaggerRoute = require('./src/routes/swagger.route')
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
@@ -13,8 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+app.use('/doc', swaggerRoute)
 app.use('/auth', AuthController)
 app.use('/admin', authenticateMiddleware, AdminController)
 
