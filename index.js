08434d44b14e3c8a8a632a00ec1a2abdd3e1a6fb
Luiz <seu-email@example.com>
2023-12-24 11:19:22 -0300

    final241414

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://marketplace-delta-ten.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'https://marketplace-delta-ten.vercel.app', 'https://marketplace-lmgaspa.vercel.app/signup', 'localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
