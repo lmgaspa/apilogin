00251b23c19844a8cd0ce7d589b87635c07c7deb
Luiz <seu-email@example.com>
2023-12-24 11:04:02 -0300

    final244

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const swaggerRoute = require('./src/routes/swagger.route.js')
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://marketplace-delta-ten.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 app.use('/doc', swaggerRoute)
 app.use('/auth', AuthController)
