670b46df0b9ecf89e7af94495b1cdac87941796a
Luiz <seu-email@example.com>
2024-06-03 09:11:46 -0300

    id_creation

--- src/models/User.js
+++ src/models/User.js
@@ -1,38 +1,42 @@
 const mongoose = require('../database');
 const bcryptjs = require('bcryptjs');
+const { v4: uuidv4 } = require('uuid');
 
 const UserSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        default: uuidv4,
+        unique: true,
+    },
     name: {
         type: String,
-        require: true,
+        required: true,
     },
-
     email: {
         type: String,
-        require: true,
+        required: true,
         unique: true,
         lowercase: true,
     },
-
     password: {
         type: String,
-        require: true,
+        required: true,
         select: false,
     },
-    
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
     }
 });
 
 UserSchema.pre('save', async function(next) {
-    const hash = await bcryptjs.hash(this.password, 10);
-    console.log(this);
-    console.log(hash);
-    this.password = hash;
-})
+    if (this.isModified('password')) {
+        const hash = await bcryptjs.hash(this.password, 10);
+        this.password = hash;
+    }
+    next();
+});
 
 const User = mongoose.model('User', UserSchema);
 
-module.exports = User
+module.exports = User;
