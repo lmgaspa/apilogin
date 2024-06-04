cfa3ef79541bf5d099ce2ecfb03594d7e6536405
Luiz <seu-email@example.com>
2024-06-04 12:31:02 -0300

    rem

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://diana-global.vercel.app/',
+    res.header('Access-Control-Allow-Origin: *', 'https://diana-global.vercel.app/',
     'https://diana-global.vercel.app/api/auth/', 'https://diana-global.vercel.app/api/auth/', 'localhost:3000', 'localhost:3000/signup');
+    
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
