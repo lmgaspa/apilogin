532fbbf0467ddeede22d5b7faafff66e87dceb22
Luiz <seu-email@example.com>
2024-06-04 11:59:31 -0300

    dg

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://marketplace-delta-ten.vercel.app', 'https://marketplace-lmgaspa.vercel.app/signup', 'localhost:3000', 'localhost:3000/signup');
+    res.header('Access-Control-Allow-Origin', 'https://diana-global.vercel.app/', 'localhost:3000', 'localhost:3000/signup');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
