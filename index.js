285317b644e6bb152b26a077cd5004610d8370db
Luiz <seu-email@example.com>
2024-06-15 01:53:37 -0300

    cors

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
 const express = require('express');
 const AuthController = require('./src/controllers/AuthController.js');
-const AdminController = require('./src/controllers/AdminController')
-
-const authenticateMiddleware = require('./src/middlewares/authenticate')
+const AdminController = require('./src/controllers/AdminController');
+const authenticateMiddleware = require('./src/middlewares/authenticate');
 const cors = require('cors');
-const swaggerRoute = require('./src/routes/swagger.route.js')
+const swaggerRoute = require('./src/routes/swagger.route.js');
 
 const app = express();
-app.use(cors());
+
+// Definição das origens permitidas
+const allowedOrigins = [
+    'https://diana-global.vercel.app',
+    'https://dianaglobal.com.br',
+    'http://localhost:3000'
+];
+
+// Configuração do middleware CORS
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+// Aplicação do middleware CORS
+app.use(cors(corsOptions));
 app.use(express.json());
+
+// Remoção do middleware CORS redundante
+/*
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin: *', 'https://diana-global.vercel.app/', 'dianaglobal.com.br', 'https://dianaglobal.com.br',
-    'https://diana-global.vercel.app/api/auth/', 'https://diana-global.vercel.app/api/auth/', 'localhost:3000', 'localhost:3000/signup');
-    
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-  });
+});
+*/
 
-app.use('/doc', swaggerRoute)
-app.use('/auth', AuthController)
-app.use('/admin', authenticateMiddleware, AdminController)
+app.use('/doc', swaggerRoute);
+app.use('/auth', AuthController);
+app.use('/admin', authenticateMiddleware, AdminController);
 
 app.listen(3001, () => {
-    console.log('Server is running');
-})
+    console.log('Server is running on port 3001');
+});
