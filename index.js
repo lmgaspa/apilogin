75248b3b34d26b1c2cf7808d955024eb0dac95d7
Luiz <seu-email@example.com>
2024-06-22 12:11:32 -0300

    readme

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin: *', 'https://dianaglobal.com.br', 'localhost:3000');
+    res.header('Access-Control-Allow-Origin: *', 'https://dianaglobal.com.br', 'localhost:3000', 'https://apilogin-mvf1.onrender.com/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
