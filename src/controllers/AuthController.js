4c15d9bdd32cdab6169f971d6d9cbe45d15b77c8
Luiz <seu-email@example.com>
2024-06-22 10:41:22 -0300

    secretenv

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const authConfig = require('../config/auth.json');
+const authConfig = require('./authConfig');
 const UserModel = require('../models/User');
 
 const router = express.Router();
