15dd6a3ab4c7532895d6bafb30fc2b0d3c86ecc9
Luiz <seu-email@example.com>
2024-06-24 19:51:29 -0300

    fix

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const AuthController = require('./src/controllers/AuthController');
 const AdminController = require('./src/controllers/AdminController');
 const authenticateMiddleware = require('./src/middlewares/authenticate');
-const cors = require('cors');
 const swaggerRoute = require('./src/routes/swagger.route.js');
 
 const app = express();
 
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://dianaglobal.com.br');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');    
-    next();
-});
-
 app.use(express.json());
+
 app.use('/doc', swaggerRoute);
 app.use('/auth', AuthController);
 app.use('/admin', authenticateMiddleware, AdminController);
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+
+app.listen(3001, () => {
+    console.log('Server is running');
 });
