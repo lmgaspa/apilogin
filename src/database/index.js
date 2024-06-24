644a9208b5d23b8bf501ebdef3b10abdd70822b8
Luiz <seu-email@example.com>
2024-06-24 20:06:20 -0300

    fix

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', true);
 
-mongoose.connect(process.env.MONGODB_URI, {}, (error) => {
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000
+}, (error) => {
     if (error) {
         console.log('Failed to authenticate with mongodb');
         console.log(error);
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('../database/index');
 const bcryptjs = require('bcryptjs');
 const { v4: uuidv4 } = require('uuid');
 const Schema = mongoose.Schema;
