d3c822af8c4220e0fb77da65d75bdca47683e869
Luiz <seu-email@example.com>
2024-06-25 15:19:41 -0300

    changes

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGO_URI;
+const uri = process.env.MONGODB_URI;
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
