84f298adaba11ec917f6ba979b34fb434aaca2a4
Luiz <seu-email@example.com>
2024-06-25 14:40:31 -0300

    changes

--- src/database/index.js
+++ src/database/index.js
@@ -5,16 +5,16 @@ const mongoose = require('mongoose');
 const uri = process.env.MONGO_URI;
 
 mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Conexão com mongodb estável');
+})
+.catch((error) => {
+    console.log('Falha ao autenticar com mongodb');
+    console.log(error);
 });
-
-(error) => {
-    if (error) {
-        console.log('Falha ao autenticar com mongodb');
-        console.log(error);
-        return;
-    }
-    console.log('Conexão com mongodb estável')
-}
 
 mongoose.Promise = global.Promise;
 
