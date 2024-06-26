5a9825c0414008291842a5446032813ddcd3438d
Luiz <seu-email@example.com>
2024-06-26 01:26:45 -0300

    cry

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
-
 const mongoose = require('mongoose');
 
 const uri = process.env.MONGODB_URI;
+
+// Set the strictQuery option to prepare for Mongoose 7
+mongoose.set('strictQuery', true); // Or false, depending on your preference
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
