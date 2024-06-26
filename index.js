44ec946aabf2a75fa6d36d33b26c3e5640cbf8e7
Luiz <seu-email@example.com>
2024-06-26 14:20:24 -0300

    fix

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const AuthController = require('./src/controllers/AuthController.js');
 const AdminController = require('./src/controllers/AdminController');
 const authenticateMiddleware = require('./src/middlewares/authenticate');
 const swaggerRoute = require('./src/routes/swagger.route.js');
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.options('*', cors()); // Enable pre-flight across all routes
+
+// CORS headers setup
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Permite solicitações de qualquer origem
+  res.header('Access-Control-Allow-Origin', 'http://example.com'); // Replace with your allowed origin
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
   if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-      return res.status(200).json({});
+      return res.status(200).send();
   }
 
   next();
 });
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://luizgabi:luizgabi0410@apiluiz.ihmeusb.mongodb.net/?appName=APILuiz";
+// MongoDB connection
+const uri = "mongodb+srv://luizgabi:luizgabi0410@apiluiz.ihmeusb.mongodb.net/mydatabase?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -36,24 +39,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-run().catch(console.dir);
 
+run().catch(console.error);
+
+// Routes
 app.use('/doc', swaggerRoute);
 app.use('/auth', AuthController);
 app.use('/admin', authenticateMiddleware, AdminController);
 
+// Server start
 const port = process.env.PORT || 10000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-  
