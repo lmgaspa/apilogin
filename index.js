ac419a1d5bcefcff481691b64d062c6a53c205db
Luiz <seu-email@example.com>
2024-06-26 00:10:18 -0300

    4

--- index.js
+++ index.js
@@ -32,12 +32,6 @@ connectToMongoDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin: *', 'https://dianaglobal.com.br', 'localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
 
 app.use('/doc', swaggerRoute)
 app.use('/auth', AuthController)
