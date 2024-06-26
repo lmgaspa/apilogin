f2176a26b03d4e62a28dfe0eb124e6844baf995f
Luiz <seu-email@example.com>
2024-06-26 15:43:55 -0300

    spanishenv

--- src/database/index.js
+++ src/database/index.js
@@ -1,20 +1,15 @@
-require('dotenv').config();
+const mongoose = require('mongoose')
 
-const mongoose = require('mongoose');
-
-const uri = process.env.MONGODB_URI;
-
-mongoose.connect(uri, {
-});
-
-(error) => {
-    if (error) {
-        console.log('Falha ao autenticar com mongodb');
-        console.log(error);
-        return;
+mongoose.connect('mongodb+srv://luizgabi:luizgabi0410@apiluiz.ihmeusb.mongodb.net/api-nodejs-mongo?retryWrites=true&w=majority', {},
+    (error) => {
+    if(error) {
+console.log('Falha ao autenticar com mongodb');
+console.log(error);
+return;
     }
+
     console.log('Conexão com mongodb estável')
-}
+})
 
 mongoose.Promise = global.Promise;
 
