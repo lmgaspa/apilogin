627ebab4e3ee713e63f1cc9fe7b2abef4f068eea
Luiz <seu-email@example.com>
2024-06-26 03:32:06 -0300

    crycs

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const cors = require('cors');
+const app = express();
 const AuthController = require('./src/controllers/AuthController.js');
 const AdminController = require('./src/controllers/AdminController');
 const authenticateMiddleware = require('./src/middlewares/authenticate');
 const swaggerRoute = require('./src/routes/swagger.route.js');
 
-const app = express();
-app.use(express.json());
-
-// Custom middleware to set headers
-app.use(cors(corsOptions));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Allow all origins or specify specific origins
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  // Responder às preflight requests (OPÇÕES)
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+}
+
+next();
 });
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
