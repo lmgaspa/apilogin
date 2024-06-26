cdfc4cdad6e09e7acd7adc821ec620bc4212cd1f
Luiz <seu-email@example.com>
2024-06-26 15:30:56 -0300

    spanishenv

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
 const express = require('express');
-const app = express();
-const cors = require('cors');
 const AuthController = require('./src/controllers/AuthController.js');
-const AdminController = require('./src/controllers/AdminController');
-const authenticateMiddleware = require('./src/middlewares/authenticate');
-const swaggerRoute = require('./src/routes/swagger.route.js');
-const client = require('./src/database/db')
+const AdminController = require('./src/controllers/AdminController')
 
-client.connect()
-    .then(() => {
-        console.log('Successfully connected to MongoDB Atlas');
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB Atlas:', error);
-    });
-;
+const authenticateMiddleware = require('./src/middlewares/authenticate')
+const cors = require('cors');
+const swaggerRoute = require('./src/routes/swagger.route.js')
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const app = express();
 app.use(cors());
-app.options('*', cors()); // Enable pre-flight across all routes
-
-// CORS headers setup
+app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Origin: *', 'https://dianaglobal.com.br', 'localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
-  if (req.method === 'OPTIONS') {
-    return res.status(200).send();
-  }
+app.use('/doc', swaggerRoute)
+app.use('/auth', AuthController)
+app.use('/admin', authenticateMiddleware, AdminController)
 
-  next();
-});
-
-  app.use('/doc', swaggerRoute);
-  app.use('/auth', AuthController);
-  app.use('/admin', authenticateMiddleware, AdminController);
+app.listen(3001, () => {
+    console.log('Server is running');
+})
---
+++ src/database/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, {
+});
+
+(error) => {
+    if (error) {
+        console.log('Falha ao autenticar com mongodb');
+        console.log(error);
+        return;
+    }
+    console.log('Conexão com mongodb estável')
+}
+
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
