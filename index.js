6249e316a9040e4e8026fe2dc3d1403623126e06
Luiz <seu-email@example.com>
2024-06-25 23:49:32 -0300

    4

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const AuthController = require('./src/controllers/AuthController.js');
 const AdminController = require('./src/controllers/AdminController.js')
-
 const authenticateMiddleware = require('./src/middlewares/authenticate.js')
 const cors = require('cors');
 const swaggerRoute = require('./src/routes/swagger.route.js')
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+
+async function connectToMongoDB() {
+    const client = new MongoClient(uri, {
+    });
+
+    try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+  } finally {
+      // Ensure the client is closed when you finish using it
+      await client.close();
+      console.log('Disconnected from MongoDB');
+  }
+}
+
+connectToMongoDB();
 
 const app = express();
 app.use(cors());
