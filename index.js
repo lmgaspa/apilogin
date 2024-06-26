d19a339e3ff7d9d12aeceaf79113a8d838804e7f
Luiz <seu-email@example.com>
2024-06-26 06:19:22 -0300

    new

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const cors = require('cors');
 const AuthController = require('./src/controllers/AuthController.js');
 const AdminController = require('./src/controllers/AdminController');
@@ -9,7 +10,7 @@ const swaggerRoute = require('./src/routes/swagger.route.js');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-router.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Permite solicitações de qualquer origem
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
