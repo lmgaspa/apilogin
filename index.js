82ed5588ecebff776d2f0d37e186fdd97026f118
Luiz <seu-email@example.com>
2024-06-26 01:06:35 -0300

    follow

--- index.js
+++ index.js
@@ -9,12 +9,17 @@ const swaggerRoute = require('./src/routes/swagger.route.js')
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin: *', 'https://dianaglobal.com.br', 'localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 app.use('/doc', swaggerRoute)
 app.use('/auth', AuthController)
 app.use('/admin', authenticateMiddleware, AdminController)
 
 app.listen(10000, () => {
-    console.log('Server is running babe');
+    console.log('Server is running');
 })
