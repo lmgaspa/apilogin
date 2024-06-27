d8833de10761a1ff8afc4b5684ea0d9301118cdb
Luiz <seu-email@example.com>
2024-06-26 23:21:17 -0300

    clientenv

--- src/database/index.js
+++ src/database/index.js
@@ -1,17 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
 
 async function connectDB() {
-    await mongoose.connect('mongodb+srv://luizgabi:luizgabi0410@apiluiz.ihmeusb.mongodb.net/api-nodejs-mongo?retryWrites=true&w=majority', {},
-        (error) => {
-            if (error) {
-                console.log('Falha ao autenticar com mongodb');
-                console.log(error);
-                return;
-            }
-            console.log('Conexão com mongodb estável')
-        })
+  try {
+    await client.connect();
+    const dbRole = await client.db().command({ hello: 1 });
+    console.log(
+      `Role of database - Host: ${dbRole.me}  Is primary: ${dbRole.isWritablePrimary}`
+    );
+    await client.close();
+    
+    // Mongoose connection
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Mongoose connected');
+  } catch (e) {
+    console.log('Error: ', e.message);
+  }
 }
 
 mongoose.Promise = global.Promise;
 
-module.exports = connectDB
+module.exports = connectDB;
