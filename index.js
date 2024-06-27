290e1c987febcc8968ed0e8f98cc465dc4639853
Luiz <seu-email@example.com>
2024-06-26 22:32:52 -0300

    new

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const AuthController = require('./src/controllers/AuthController.js');
 const AdminController = require('./src/controllers/AdminController')
-
 const authenticateMiddleware = require('./src/middlewares/authenticate')
 const cors = require('cors');
 const swaggerRoute = require('./src/routes/swagger.route.js')
-
 const app = express();
+const connectDB = require('./src/database/index.js')
+
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
@@ -15,6 +15,8 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
+
+  connectDB();
 
 app.use('/doc', swaggerRoute)
 app.use('/auth', AuthController)
--- src/database/index.js
+++ src/database/index.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://luizgabi:luizgabi0410@apiluiz.ihmeusb.mongodb.net/api-nodejs-mongo?retryWrites=true&w=majority', {},
-    (error) => {
-    if(error) {
-console.log('Falha ao autenticar com mongodb');
-console.log(error);
-return;
-    }
-
-    console.log('Conexão com mongodb estável')
-})
+async function connectDB() {
+    await mongoose.connect('mongodb+srv://luizgabi:luizgabi0410@apiluiz.ihmeusb.mongodb.net/api-nodejs-mongo?retryWrites=true&w=majority', {},
+        (error) => {
+            if (error) {
+                console.log('Falha ao autenticar com mongodb');
+                console.log(error);
+                return;
+            }
+            console.log('Conexão com mongodb estável')
+        })
+}
 
 mongoose.Promise = global.Promise;
 
-module.exports = mongoose;
+module.exports = connectDB
